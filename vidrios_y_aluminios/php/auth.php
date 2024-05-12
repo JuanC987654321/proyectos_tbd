470b13579b360e6296bffeac3e8a385acb25eda3
JuanC987654321 <135085698+JuanC987654321@users.noreply.github.com>
2024-05-11 20:11:34 -0600

    Add files via upload

--- vidrios_y_aluminios/php/auth.php
+++ vidrios_y_aluminios/php/auth.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION["id_staff"])) {
     echo "<script>alert('NO SE A ACCEDIDO A ESTA PAGINA DE MANERA CORRECTA, SERA REDIRECCIONADO A LA PAGINA DE INICIO DE SESION');window.location='index.php';</script>";
 }else{
     if(isset($_SESSION["Role"])){
-        echo("hay rol asignado a este usuario '" . $_SESSION["Role"] . "' :D, eso es todo lo que se comprueba de momento pero las bases ya estan");
+        echo("" . $_SESSION["Role"] . "");
     }
 }
 ?>
