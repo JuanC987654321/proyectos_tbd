80ea080ff461700fd99ad697c123b56b49bfcd6a
JuanC987654321 <135085698+JuanC987654321@users.noreply.github.com>
2024-05-11 20:04:45 -0600

    Add files via upload

---
+++ vidrios_y_aluminios/rastrear.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link href="styles/style.css" rel="stylesheet">
+    <link href="styles/rastrear.css" rel="stylesheet">
+    <link href="styles/normalize.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    <?php
+        require_once "php/barraLateral.php";
+    ?>
+
+    <main>
+    <form action="php/s_folio.php" method="post">
+        
+        <img class="logo" src="images/logo.png" width="200" alt="Foto del logo" >
+
+        <fieldset>
+            <label class="label">NÚMERO DE FOLIO:</label>
+            <input type="text" id="text" name="folio" required placeholder="numero de FOLIO:">
+            <input type="submit" value="Ingresar"> 
+        </fieldset>
+    </form> 
+    </main>
+        
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+
+</body>
+</html>
