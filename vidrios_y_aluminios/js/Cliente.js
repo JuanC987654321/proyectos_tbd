886f2e1d371bb9109ee36e06d7a8a3ab86afef41
JuanC987654321 <135085698+JuanC987654321@users.noreply.github.com>
2024-05-13 00:39:04 -0600

    Add files via upload

---
+++ vidrios_y_aluminios/js/Cliente.js
@@ -0,0 +1,11 @@
+function bajarCotizacion() {
+    alert("Funcionalidad de bajar cotización en proceso...");
+}
+
+// Establecer la fecha y hora del pedido (ejemplo: 1 de enero de 2023 a las 10:30 AM)
+const fechaPedido = new Date(2024, 4, 12, 5, 53); // Año, Mes (0-11), Día, Hora, Minutos
+const options = { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit' };
+const fechaPedidoFormateada = fechaPedido.toLocaleDateString('es-ES', options);
+
+// Mostrar la fecha y hora del pedido
+document.getElementById('fechaPedido').innerText = fechaPedidoFormateada;
