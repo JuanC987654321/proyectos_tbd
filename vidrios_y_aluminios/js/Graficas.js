1ee5e85f1e4a83e932fb86c5c2887e805e299f56
JuanC987654321 <135085698+JuanC987654321@users.noreply.github.com>
2024-05-17 16:09:34 -0600

    cambiado para poder ser llamado externamente

--- vidrios_y_aluminios/js/Graficas.js
+++ vidrios_y_aluminios/js/Graficas.js
@@ -1,24 +1,28 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//dibujarGrafica(60, 30, 20, 10, 5);
+function dibujarGrafica(cinco, cuatro, tres, dos, una,){
     const ctx = document.getElementById('salesChart').getContext('2d');
     const salesChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['Producto A', 'Producto B', 'Producto C', 'Producto D'],
+            labels: ['5 Estrella', '4 Estrellas', '3 Estrellas', '2 Estrellas', '1 Estrellas'],
             datasets: [{
-                label: 'Unidades vendidas',
-                data: [200, 150, 180, 220],
+                label: 'Servicios con esta calificacion',
+                data: [cinco, cuatro, tres, dos, una],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)'
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(200, 59, 160, 0.2)'
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)'
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(200, 59, 160, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -31,4 +35,6 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
-});
+// });
+};
+
