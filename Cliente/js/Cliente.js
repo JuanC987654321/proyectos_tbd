f2be4cc806c8eee7d2ccef418772d981c56b1925
JuanC987654321 <135085698+JuanC987654321@users.noreply.github.com>
2024-05-24 00:44:18 -0600

    Delete Cliente directory

--- Cliente/js/Cliente.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('folioForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const folio = document.getElementById('folioInput').value;
-    window.location.href = `/Cliente/html/Progreso.html?folio=${folio}`;
-});
