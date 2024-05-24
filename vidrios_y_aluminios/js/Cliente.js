a7bba4ca8829b1b151fb8ec99479c246d442aa06
JuanC987654321 <135085698+JuanC987654321@users.noreply.github.com>
2024-05-24 01:03:27 -0600

    commit

--- vidrios_y_aluminios/js/Cliente.js
+++ vidrios_y_aluminios/js/Cliente.js
@@ -2,6 +2,11 @@ function bajarCotizacion() {
     alert("Funcionalidad de bajar cotización en proceso...");
 }
 
+function estadoCuenta() {
+    alert("Funcionalidad de estado de cuenta en proceso...");
+}
+
+
 // // Establecer la fecha y hora del pedido (ejemplo: 1 de enero de 2023 a las 10:30 AM)
 // const fechaPedido = new Date(2024, 4, 12, 5, 53); // Año, Mes (0-11), Día, Hora, Minutos
 // const options = { day: '2-digit', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit' };
